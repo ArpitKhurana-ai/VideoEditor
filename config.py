0f24d70aab7bee58eb826e7094eb6befe5ae9265
ArpitKhurana-ai <arpitkhurana155@gmail.com>
2025-05-24 15:52:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,10 @@ import os
 
 class Config:
     VIDEO_DIR = os.path.join("static", "outputs")
-    COOKIE_PATH = os.path.join("cookies", "cookies.txt")
-
     os.makedirs(VIDEO_DIR, exist_ok=True)
 
     PORT = 8188
     HOST = "0.0.0.0"
     DEBUG = True
 
-    SECRET_KEY = os.environ.get("SESSION_SECRET", "youtube-trim-api-secret-key")
+    SECRET_KEY = os.environ.get("SESSION_SECRET", "youtube-trim-api")
